01fad5e255b38b62097918404545de9aaf7f5b4f
Jeff Stanley <10554861+JS-Law@users.noreply.github.com>
2024-07-03 22:31:53 -0400

    Add css loaders and added info to configs

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,22 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: 'asset/resource',
+            },
+            {
+                test: /\.('woff|woff2|eot|ttf|otf')$/i,
+                use: 'asset/resource',
+            }
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             inject: 'body',
